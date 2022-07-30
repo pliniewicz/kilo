d478718e5d71dc8b378cb8d6b799b1dfc29443fd
pliniewicz <jazingek@gmail.com>
2022-07-30 21:07:37 +0200

    double checks for window size

--- kilo.c
+++ kilo.c
@@ -28,8 +28,7 @@ void die(const char *s){
 }
 
 void disableRawMode(){
-	if	(tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1);
-		die("tcsetattr");
+	if	(tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) die("tcsetattr");
 }
 
 void enableRawMode(){
@@ -57,17 +56,52 @@ void enableRawMode(){
 char editorReadKey() {
 	int nread;
 	char c;
-	while ((nread) = read(STDIN_FILENO, &c, 1) != 1) {
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
 		if (nread == -1 && errno != EAGAIN) die("read");
 	}
 	return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+	char buf[32];
+	unsigned int i = 0;
+
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+	while (i < sizeof(buf) -1) {
+		if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+		if (buf[i] == 'R') break;
+		i++;
+	}
+	buf[i] = '\0';
+
+	if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+	if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+	/* printf("\r\n&buf[1]: '%s'\r\n", &buf[1]); */
+
+	/* editorReadKey(); */
+	/* return -1; */
+	return 0;
+
+	/* printf("\r\n"); */
+	/* char c; */
+	/* while (read(STDIN_FILENO, &c, 1) == 1){ */
+		/* if (iscntrl(c)) { */
+			/* printf("%d\r\n", c); */
+		/* } else { */
+			/* printf("%d ('%c')\r\n", c, c); */
+		/* } */
+	/* } */
+}
+
 int getWindowSize(int *rows, int *cols){
 	struct winsize ws;
 
-	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col ==0) {
-		return -1;
+	if (/* 1 || */ ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col ==0) {
+		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+		/* editorReadKey(); */
+		/* return -1; */
+		return getCursorPosition(rows, cols);
 	} else {
 		*cols = ws.ws_col;
 		*rows = ws.ws_row;
