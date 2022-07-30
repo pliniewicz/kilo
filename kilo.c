b4baa4809bc5474de4a0654a35a347dd18078940
pliniewicz <jazingek@gmail.com>
2022-07-30 11:43:07 +0200

    raw mode now comes back to normal at exit

--- kilo.c
+++ kilo.c
@@ -1,20 +1,36 @@
+#include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
 
+struct termios orig_termios;
+
+void disableRawMode(){
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode(){
-	struct termios raw;
 
-	tcgetattr(STDIN_FILENO, &raw);
+	tcgetattr(STDIN_FILENO, &orig_termios); // get attributes of original mode
 
-	raw.c_lflag &= ~(ECHO);
+	atexit(disableRawMode); // when exiting the program, call disable function
 
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	struct termios raw = orig_termios; // create a struct from termios && copy orig into the raw struct
+
+	tcgetattr(STDIN_FILENO, &raw); // read terminal attributes
+
+	/*******************************************************************************************************************
+	*  bitwise NOT (~) on echo, which is a bitwise map and then bitwise AND with flags to change only the fourth bit 	 *
+	********************************************************************************************************************/
+
+	raw.c_lflag &= ~(ECHO); // modify not to print, what user is typing; c_lflag is "local flag" or "miscellaneous flag"
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); // apply changes in attributes (TCSAFLUSH is when to apply the changes)
 }
 
 int main()
 {
-	enableRawMode();
+	enableRawMode(); // call raw mode
 	char c;
-	while	(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	while	(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'); // listen to input until 'q' char appears, otherwise load it into 'c' variable
 	return 0;
 }
